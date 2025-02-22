d7f03e07499052e4f7426dc9a35dd4a406954f39
Ilya <prosoulk2017@gmail.com>
2025-02-22 08:49:26 +0100

    init project

---
+++ src/main/java/com/alakey/CryptoRecommendationsService/CryptoRecommendationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alakey.CryptoRecommendationsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommendationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecommendationsServiceApplication.class, args);
+	}
+
+}
